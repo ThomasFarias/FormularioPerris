021d36336641c89a8080ee7fbf2b5ade2a556e12
ThomasFarias <42778501+ThomasFarias@users.noreply.github.com>
2018-09-09 21:17:39 -0300

    Regiones y provincias en 2 listas

--- js/codigo.js
+++ js/codigo.js
@@ -67,3 +67,120 @@ $(document).ready(function(){
     return true;
   });
 });
+
+
+//Genera una lista desplegable de provincias dependiendo de la region seleccionada
+$(document).ready(function() {
+  
+  $("#region").change(function() {
+    
+    var reg = $(this) ;
+      $("#prov option").remove() ; 
+    if(reg.val() === "ARICA Y PARINACOTA" ) {
+ 
+    $("#prov").append("<option>Arica</option>");    
+    $("#prov").append("<option>Parinacota</option>");
+    }
+      else if(reg.val() === "TARAPACÁ" ) {
+          
+    $("#prov").append("<option>Iquique</option>");    
+    $("#prov").append("<option>Tamarugal</option>"); }
+ 
+
+
+    
+else if(reg.val() === "ANTOFAGASTA" ) {
+          
+    $("#prov").append("<option>Antofagasta</option>");    
+    $("#prov").append("<option>El loa</option>");    
+    $("#prov").append("<option>Tocopilla</option>"); }
+  
+ else if(reg.val() === "ATACAMA" ) {
+          
+    $("#prov").append("<option>Copiapo</option>");    
+    $("#prov").append("<option>Chañaral</option>");    
+    $("#prov").append("<option>Tocopilla</option>"); }
+
+ else if(reg.val() === "COQUIMBO" ) {
+          
+    $("#prov").append("<option>Elqui</option>");    
+    $("#prov").append("<option>Choapa</option>");     
+    $("#prov").append("<option>Limari</option>"); }
+
+ else if(reg.val() === "VALPARAÍSO" ) {
+          
+    $("#prov").append("<option>Valparaíso</option>");    
+    $("#prov").append("<option>Isla de Pascua</option>");    
+    $("#prov").append("<option>San Antonio</option>");
+    $("#prov").append("<option>San Felipe de Aconcagua</option>");   }
+ 
+else if(reg.val() === "LIBERTADOR GRAL. BERNARDO O'HIGGINS" ) {
+          
+    $("#prov").append("<option>Cachapoal</option>");    
+    $("#prov").append("<option>Cardenal Caro</option>");    
+    $("#prov").append("<option>Colchagua</option>");   }
+
+else if(reg.val() === "MAULE" ) {
+          
+    $("#prov").append("<option>Talca</option>");    
+    $("#prov").append("<option>Cauquenes</option>");    
+    $("#prov").append("<option>Curicó</option>");
+    $("#prov").append("<option>Linares</option>");   }
+ 
+else if(reg.val() === "BIOBÍO" ) {
+          
+    $("#prov").append("<option>Concepción</option>");    
+    $("#prov").append("<option>Biobío</option>");    
+    $("#prov").append("<option>Ñuble</option>");
+   }
+ 
+else if(reg.val() === "ARAUCANÍA" ) {
+          
+    $("#prov").append("<option>Cautín</option>");    
+    $("#prov").append("<option>Malleco</option>");    
+      }
+ 
+else if(reg.val() === "DE LOS RÍOS" ) {
+          
+    $("#prov").append("<option>Valdivia</option>");    
+    $("#prov").append("<option>Ranco</option>");    
+
+   }
+  
+else if(reg.val() === "DE LOS LAGOS" ) {
+          
+    $("#prov").append("<option>Llanquihue</option>");    
+    $("#prov").append("<option>Chiloé</option>");    
+    $("#prov").append("<option>Osorno</option>"); 
+    $("#prov").append("<option>Palena</option>");
+   }
+
+else if(reg.val() === "AYSÉN" ) {
+          
+    $("#prov").append("<option>Coyhaique</option>");    
+    $("#prov").append("<option>Aysén</option>");    
+    $("#prov").append("<option>Capitán Prat</option>"); 
+    $("#prov").append("<option>General Carrera</option>");
+   }
+ 
+else if(reg.val() === "MAGALLANES Y DE LA ANTÁRTICA CHILENA" ) {
+          
+    $("#prov").append("<option>Magallanes</option>");    
+    $("#prov").append("<option>Antártica Chilena</option>");    
+    $("#prov").append("<option>Tierra del Fuego</option>"); 
+    $("#prov").append("<option>Ultima Esperanza</option>");
+   }
+ 
+else if(reg.val() === "METROPOLITANA DE SANTIAGO" ) {
+          
+    $("#prov").append("<option>Santiago</option>");    
+    $("#prov").append("<option>Cordillera</option>");    
+    $("#prov").append("<option>Chacabuco</option>"); 
+    $("#prov").append("<option>Maipo</option>");
+   }
+  });
+   
+
+  
+  
+});
